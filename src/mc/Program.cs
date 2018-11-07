c2c78b820543cdce5cabb2941a2b1c0ab317e54d
Immo Landwerth <immo@landwerth.net>
2018-11-07 08:29:32 -0800

    Add colorization to REPL

--- src/mc/Program.cs
+++ src/mc/Program.cs
@@ -19,10 +19,14 @@ namespace Minsk
 
             while (true)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
+
                 if (textBuilder.Length == 0)
-                    Console.Write("> ");
+                    Console.Write("» ");
                 else
-                    Console.Write("| ");
+                    Console.Write("· ");
+
+                Console.ResetColor();
 
                 var input = Console.ReadLine();
                 var isBlank = string.IsNullOrWhiteSpace(input);
@@ -66,7 +70,9 @@ namespace Minsk
 
                 if (!result.Diagnostics.Any())
                 {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
                     Console.WriteLine(result.Value);
+                    Console.ResetColor();
                 }
                 else
                 {
